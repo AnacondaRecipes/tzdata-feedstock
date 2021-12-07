65f52b40415a16318e9fabc9a6f0cfe7026dae6b
abolla22 <abolla@anaconda.com>
2021-12-07 15:39:10 -0600

    moved test case to python-tzdata pkg

--- recipe/run_test.py
+++
@@ -1,8 +0,0 @@
-try:
-    from importlib import resources
-except ImportError:
-    import importlib_resources as resources
-
-# America/New_York
-with resources.open_binary("tzdata.zoneinfo.America", "New_York") as f:
-    assert f.read(4) == b"TZif", "Failed to validate tzdata package"
