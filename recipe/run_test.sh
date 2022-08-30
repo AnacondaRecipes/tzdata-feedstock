043356b1a92df7c04be92d3be5511ca2d24d4023
Marco Esters <mesters@anaconda.com>
2022-08-30 10:22:30 -0700

    Move in-line tests to test script

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -20,3 +20,12 @@ done
 # Make sure we only package zoneinfo and nothing else.
 dirs="$(find "${PREFIX}" -mindepth 1 -maxdepth 2 ! -path "${PREFIX}/share" ! -path "${PREFIX}/conda-meta*")"
 test "${dirs}" = "${PREFIX}/share/zoneinfo"
+
+# Make sure we only package timezone information files.
+if [ `uname` == 'Darwin' ]; then
+  # OSX does not have the '-printf' operator.
+  heads="$(find "${PREFIX}/share/zoneinfo" -type f ! -name "*.zi" ! -name "*.tab" ! -name leapseconds -exec head -c4 {} ';' -print0 | uniq | cut -c1-4)"
+else
+  heads="$(find "${PREFIX}/share/zoneinfo" -type f ! -name "*.zi" ! -name "*.tab" ! -name leapseconds -exec head -c4 {} ';' -printf \\n | uniq)"
+fi
+test "${heads}" = TZif
